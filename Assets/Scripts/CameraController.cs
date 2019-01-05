7f1398975b2a39eaa20223bf2b67d5c8943da01c
JensenJ <jensenbradley@icloud.com>
2019-01-05 20:11:34 +0000

    Map Generator Created

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,170 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public LayerMask groundLayer;
+    public float raycastDistance = 1000f;
+
+    [System.Serializable]
+    public class PositionSettings
+    {
+        public bool invertPan = true;
+        public float panSmooth = 7f;
+        public float distanceFromGround = 400;
+        public bool allowZoom = true;
+        public float zoomSmooth = 5;
+        public float zoomStep = 5;
+        public float maxZoom = 25;
+        public float minZoom = 1000;
+        public float minPan = -1800f;
+        public float maxPan = 1000;
+        public float newDistance = 400;
+    }
+
+    [System.Serializable]
+    public class OrbitSettings
+    {
+        public float xRot = 30;
+        public float yRot = 45;
+        public bool allowYOrbit = true;
+        public float yOrbitSmooth = 0.5f;
+    }
+
+    [System.Serializable]
+    public class InputSettings
+    {
+        public string PAN = "MousePan";
+        public string ORBIT_Y = "MouseTurn";
+        public string ZOOM = "Mouse ScrollWheel";
+    }
+
+    public PositionSettings position = new PositionSettings();
+    public OrbitSettings orbit = new OrbitSettings();
+    public InputSettings input = new InputSettings();
+
+    Vector3 destination = Vector3.zero;
+    Vector3 camVel = Vector3.zero;
+    Vector3 previousMousePos = Vector3.zero;
+    Vector3 currentMousePos = Vector3.zero;
+    float panInput, orbitInput, zoomInput;
+    int panDirection = 0;
+
+    void Start()
+    {
+        panInput = 0;
+        orbitInput = 0;
+        zoomInput = 0;
+    }
+
+    void GetInput()
+    {
+        panInput = Input.GetAxis(input.PAN);
+        orbitInput = Input.GetAxis(input.ORBIT_Y);
+        zoomInput = Input.GetAxis(input.ZOOM);
+
+        previousMousePos = currentMousePos;
+        currentMousePos = Input.mousePosition;
+    }
+
+    void Update()
+    {
+        GetInput();
+        if (position.allowZoom)
+        {
+            Zoom();
+        }
+        if (orbit.allowYOrbit)
+        {
+            Rotate();
+        }
+        PanWorld();
+    }
+
+    void FixedUpdate()
+    {
+        HandleCameraDistance();
+    }
+
+    void PanWorld()
+    {
+        Vector3 targetPos = transform.position;
+        if (position.invertPan)
+        {
+            panDirection = -1;
+        }
+        else
+        {
+            panDirection = 1;
+        }
+        if(panInput > 0)
+        {
+            if(targetPos.x > position.maxPan || targetPos.z > position.maxPan || targetPos.x < position.minPan || targetPos.z < position.minPan)
+            {
+                
+            }
+            else
+            {
+                targetPos += transform.right * (currentMousePos.x - previousMousePos.x) * position.panSmooth * panDirection * Time.deltaTime;
+                targetPos += Vector3.Cross(transform.right, Vector3.up) * (currentMousePos.y - previousMousePos.y) * position.panSmooth * panDirection * Time.deltaTime;
+            }
+        }
+        if(targetPos.x > position.maxPan)
+        {
+            targetPos.x = position.maxPan;
+        }else if (targetPos.x < position.minPan)
+        {
+            targetPos.x = position.minPan;
+        }else if(targetPos.z > position.maxPan)
+        {
+            targetPos.z = position.maxPan;
+        }else if(targetPos.z < position.minPan)
+        {
+            targetPos.z = position.minPan;
+        }
+        transform.position = targetPos;
+    }
+
+    void HandleCameraDistance()
+    {
+        Ray ray = new Ray(transform.position, transform.forward);
+        RaycastHit hit;
+        if(Physics.Raycast(ray, out hit, raycastDistance, groundLayer))
+        {
+            destination = Vector3.Normalize(transform.position - hit.point) * position.distanceFromGround;
+            destination += hit.point;
+            transform.position = Vector3.SmoothDamp(transform.position, destination, ref camVel, 0.3f);
+        }
+    }
+
+    void Zoom()
+    {
+        position.newDistance += position.zoomStep * -zoomInput;
+        position.distanceFromGround = Mathf.Lerp(position.distanceFromGround, position.newDistance, position.zoomSmooth * Time.deltaTime);
+
+        if(position.distanceFromGround < position.maxZoom)
+        {
+            position.distanceFromGround = position.maxZoom;
+            position.newDistance = position.maxZoom;
+        }
+        if(position.distanceFromGround > position.minZoom)
+        {
+            position.distanceFromGround = position.minZoom;
+            position.newDistance = position.minZoom;
+        }
+    }
+    
+    void Rotate()
+    {
+        if (Input.GetKeyUp(KeyCode.Q))
+        {
+            orbit.yRot -= 90;
+        }
+        if (Input.GetKeyUp(KeyCode.E))
+        {
+            orbit.yRot += 90;
+        }
+
+        transform.rotation = Quaternion.Euler(orbit.xRot, orbit.yRot, 0);
+
+    }
+}
---
+++ Assets/Scripts/Map/MapGenerator.cs
@@ -0,0 +1,147 @@
+﻿using UnityEngine;
+
+public class MapGenerator : MonoBehaviour
+{
+    public enum DrawMode {NoiseMap, ColourMap, Mesh};
+    public DrawMode drawMode;
+
+    public enum MapType {Islands, Boreal, Tropical};
+    public MapType mapType;
+
+    [Space(10)]
+    [SerializeField]
+    const int mapChunkSize = 241;
+    [Range(0, 6)]
+    public int levelOfDetail;
+    [Range(20, 200)]
+    public float noiseScale;
+    [Space(10)]
+    [Range(1, 6)]
+    public int octaves;
+    [Range(0, 1)]
+    public float persistance;
+    [Range(1, 4)]
+    public float lacunarity;
+    [Space(10)]
+    public int seed;
+    public Vector2 offset;
+    [Space(10)]
+    public float meshHeightMultipler;
+    public AnimationCurve meshHeightCurve;
+    [SerializeField]
+    TerrainType[] regions;
+    public MapTypeHeight[] mapHeights;
+    [Space(15)] 
+    public bool liveUpdate;
+    public bool keepSeedOnGeneration;
+
+    void Start()
+    {
+        GenerateMap();
+    }
+
+    public void SetMapType()
+    {
+        if(mapType == MapType.Islands)
+        {
+            
+            for (int i = 0; i < mapHeights.Length; i++)
+            {
+                regions[i].height = mapHeights[i].islandHeight;
+                regions[i].colour = mapHeights[i].islandColour;
+            }
+        }else if(mapType == MapType.Boreal)
+        {
+            for (int i = 0; i < mapHeights.Length; i++)
+            {
+                regions[i].height = mapHeights[i].plainsHeight;
+                regions[i].colour = mapHeights[i].plainsColour;
+
+            }
+        }else if(mapType == MapType.Tropical)
+        {
+            for (int i = 0; i < mapHeights.Length; i++)
+            {
+                regions[i].height = mapHeights[i].tropicalHeight;
+                regions[i].colour = mapHeights[i].tropicalColour;
+            }
+        }
+    }
+
+    public void GenerateMap()
+    {
+        if (!keepSeedOnGeneration)
+        {
+            seed = (int)Random.Range(-10000f, 10000f);
+        }
+        float[,] noiseMap = Noise.GenerateNoiseMap(mapChunkSize, mapChunkSize, noiseScale, seed, octaves, persistance, lacunarity, offset);
+
+        Color[] colourMap = new Color[mapChunkSize * mapChunkSize];
+        SetMapType();
+        for(int y = 0; y < mapChunkSize; y++)
+        {
+            for(int x = 0; x < mapChunkSize; x++)
+            {
+                float currentHeight = noiseMap[x, y];
+                for (int i = 0; i < regions.Length; i++)
+                {
+                    if(currentHeight <= regions[i].height)
+                    {
+                        colourMap[y * mapChunkSize + x] = regions[i].colour;
+                        break;
+                    }
+                }
+            }
+        }
+
+        MapDisplay display = FindObjectOfType<MapDisplay>();
+        if(drawMode == DrawMode.NoiseMap)
+        {
+            display.DrawTexture(TextureGenerator.TextureFromHeightMap(noiseMap));
+
+        }else if(drawMode == DrawMode.ColourMap)
+        {
+            display.DrawTexture(TextureGenerator.TextureFromColourMap(colourMap, mapChunkSize, mapChunkSize));
+        }else if(drawMode == DrawMode.Mesh)
+        {
+            display.DrawMesh(MeshGenerator.GenerateTerrainMesh(noiseMap, meshHeightMultipler, meshHeightCurve, levelOfDetail), TextureGenerator.TextureFromColourMap(colourMap, mapChunkSize, mapChunkSize));
+        }
+    }
+
+    void OnValidate()
+    {
+        if(lacunarity < 1)
+        {
+            lacunarity = 1;
+        }
+        if(octaves < 0)
+        {
+            octaves = 0;
+        }
+    }
+}
+[System.Serializable]
+public struct TerrainType
+{
+    public string name;
+    public float height;
+    public bool vegatation;
+    public float vegetationHeight;
+    public Color colour;
+}
+
+[System.Serializable]
+public struct MapTypeHeight
+{
+    public float islandHeight;
+    public float plainsHeight;
+    public float tropicalHeight;
+    [Space(20)]
+    public float islandVegetationHeight;
+    public float plainsVegetationHeight;
+    public float tropicalVegetationHeight;
+    [Space(20)]
+    public Color islandColour;
+    public Color plainsColour;
+    public Color tropicalColour;
+}
