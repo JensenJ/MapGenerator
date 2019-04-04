231839d29cb3a29a0da9f9b9a5f866a6889e21b0
JensenJ <jensenbradley@icloud.com>
2019-04-04 21:57:21 +0100

    Fixed bugs with seams on map.
    Fixed a bug where seams in one direction would not match.

--- Assets/Scripts/Map/MeshGenerator.cs
+++ Assets/Scripts/Map/MeshGenerator.cs
@@ -1,18 +1,19 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
 
 public static class MeshGenerator
 {
+
     public static MeshData GenerateTerrainMesh(float[,] heightMap, float heightMultiplier, AnimationCurve _heightCurve, int levelOfDetail)
     {
         AnimationCurve heightCurve = new AnimationCurve(_heightCurve.keys);
+
         int width = heightMap.GetLength(0);
         int height = heightMap.GetLength(1);
-        float topLeftX = (width - 1) / 2f;
+        float topLeftX = (width - 1) / -2f;
         float topLeftZ = (height - 1) / 2f;
 
-        int meshSimplificationIncrement = (levelOfDetail == 0)?1:levelOfDetail * 2;
+        int meshSimplificationIncrement = (levelOfDetail == 0) ? 1 : levelOfDetail * 2;
         int verticesPerLine = (width - 1) / meshSimplificationIncrement + 1;
 
         MeshData meshData = new MeshData(verticesPerLine, verticesPerLine);
@@ -22,18 +23,21 @@ public static class MeshGenerator
         {
             for (int x = 0; x < width; x += meshSimplificationIncrement)
             {
-                meshData.vertices[vertexIndex] = new Vector3(topLeftX - x, heightCurve.Evaluate(heightMap[x, y]) * heightMultiplier, topLeftZ - y);
-                meshData.uv[vertexIndex] = new Vector2(x / (float)width, y / (float)height);
-                if(x < width - 1 && y < height - 1)
+                meshData.vertices[vertexIndex] = new Vector3(topLeftX + x, heightCurve.Evaluate(heightMap[x, y]) * heightMultiplier, topLeftZ - y);
+                meshData.uvs[vertexIndex] = new Vector2(x / (float)width, y / (float)height);
+
+                if (x < width - 1 && y < height - 1)
                 {
-                    meshData.AddTriangles(vertexIndex, vertexIndex + verticesPerLine + 1, vertexIndex + verticesPerLine);
-                    meshData.AddTriangles(vertexIndex + verticesPerLine + 1, vertexIndex, vertexIndex + 1);
+                    meshData.AddTriangle(vertexIndex, vertexIndex + verticesPerLine + 1, vertexIndex + verticesPerLine);
+                    meshData.AddTriangle(vertexIndex + verticesPerLine + 1, vertexIndex, vertexIndex + 1);
                 }
 
                 vertexIndex++;
             }
         }
+
         return meshData;
+
     }
 }
 
@@ -41,22 +45,22 @@ public class MeshData
 {
     public Vector3[] vertices;
     public int[] triangles;
-    public Vector2[] uv;
+    public Vector2[] uvs;
 
     int triangleIndex;
 
     public MeshData(int meshWidth, int meshHeight)
     {
         vertices = new Vector3[meshWidth * meshHeight];
-        uv = new Vector2[meshWidth * meshHeight];
+        uvs = new Vector2[meshWidth * meshHeight];
         triangles = new int[(meshWidth - 1) * (meshHeight - 1) * 6];
     }
 
-    public void AddTriangles(int a, int b, int c)
+    public void AddTriangle(int a, int b, int c)
     {
-        triangles[triangleIndex] = c;
-        triangles[triangleIndex+1] = b;
-        triangles[triangleIndex+2] = a;
+        triangles[triangleIndex] = a;
+        triangles[triangleIndex + 1] = b;
+        triangles[triangleIndex + 2] = c;
         triangleIndex += 3;
     }
 
@@ -65,7 +69,7 @@ public class MeshData
         Mesh mesh = new Mesh();
         mesh.vertices = vertices;
         mesh.triangles = triangles;
-        mesh.uv = uv;
+        mesh.uv = uvs;
         mesh.RecalculateNormals();
         return mesh;
     }
